8e62408fb61b2f711d47ad9f33e75c70acb773e2
AgustinDiaz14 <67121345+AgustinDiaz14@users.noreply.github.com>
2024-12-02 17:58:24 -0300

    Do not rerun sort

--- day_one.py
+++ day_one.py
@@ -9,9 +9,10 @@ def calculate_distances(filepath):
         first_col.append(int(columns[0]))
         second_col.append(int(columns[1].replace("\n", "")))
 
+    sorted_first = sorted(first_col)
+    sorted_second = sorted(second_col)
+
     for i in range(len(first_col)):
-        sorted_first = sorted(first_col)
-        sorted_second = sorted(second_col)
         partial_diff = abs(sorted_first[i] - sorted_second[i])
         total_sum += partial_diff
     file.close()
